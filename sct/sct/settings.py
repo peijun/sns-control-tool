77be91a45e1db478adfad641ab0d6d7e141dc0d0
Jumpei Funayama <contact.zouri.music@gmail.com>
2021-07-19 22:54:42 +0900

    EDIT: app setting

--- sct/sct/settings.py
+++ sct/sct/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'snscontrol.apps.SnscontrolConfig',
+    'usercontrol.apps.UsercontrolConfig',
 ]
 
 MIDDLEWARE = [
