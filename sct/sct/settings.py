2032bf0dadadc11f26936068459dc3753d2674ee
Jumpei Funayama <contact.zouri.music@gmail.com>
2021-07-19 22:53:03 +0900

    EDIT:miss

--- sct/sct/settings.py
+++ sct/sct/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snscontrol.apps.SnsConfig',
+    'snscontrol.apps.SnscontrolConfig',
 ]
 
 MIDDLEWARE = [
