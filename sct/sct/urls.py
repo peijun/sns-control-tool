5c3d47235c71249a3266c007d1e9e3213fa07dd2
Jumpei Funayama <contact.zouri.music@gmail.com>
2021-07-19 23:16:39 +0900

    ADD:user auth urls

--- sct/sct/urls.py
+++ sct/sct/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sc/', include('polls.urls')),
+    path('sc/', include('snscontrol.urls')),
+    path('user/', include('django.contrib.auth.urls')),
 ]
